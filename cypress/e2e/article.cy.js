79b750d8d9a722aedfd5583ce9469a8af93ed05f
mrsvolodya <volodyamurskyi@gmail.com>
2025-07-29 22:37:31 +0300

    finish task

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,34 @@
-describe('', () => {
+describe('Conduit', () => {
+  const random = `${Date.now()}_${Math.floor(Math.random() * 100000)}`;
+  const title = `Article Title ${random}`;
+  const description = `This is a test article created at ${random}`;
+  const body = `Body content for article ${random}`;
+
+  const userName = `user${random}`;
+  const email = `testuser${random}@test.com`;
+  const password = 'Test123!';
+
   before(() => {
-
+    cy.visit('/');
+    cy.login(email, userName, password);
   });
 
-  it('', () => {
-
+  beforeEach(() => {
+    cy.visit('/');
   });
 
-  it('', () => {
-
+  it('should create and delete article', () => {
+    cy.reload();
+    cy.contains('a', 'New Article').click();
+    cy.get('[placeholder="Article Title"]').type(title);
+    cy.get('[placeholder="What\'s this article about?"]').type(description); // no change needed here since the apostrophe is escaped
+    cy.get('[placeholder="Write your article (in markdown)"]').type(body);
+    cy.get('.btn-primary').click();
+    cy.get('.container')
+      .should('contain', title)
+      .and('contain', 'Delete Article')
+      .and('contain', 'Edit Article');
+    cy.get('.article-actions').find('.btn-outline-danger').click();
+    cy.url().should('include', '/');
   });
 });
